4a1d5b7f8b3ce67d6b5dab933afc1c6e3e8df2f4
firnt <lopedamo@gmail.com>
2025-12-14 19:28:21 -0300

    Actualizaciones

--- todo/script.js
+++ todo/script.js
@@ -3,22 +3,31 @@ let todohtml = "";
 let i = 0;
 function addtarea() {
   const inputElement = document.querySelector(".input"); //selecciona el input
-  array.push(inputElement.value); //lo pone en el array
+  const inputDate = document.querySelector(".date");
+  let valor = inputElement.value;
+  let dia = inputDate.value;
 
+  array.push({
+    date: dia,
+    nombre: valor,
+  }); //lo pone en el array
+  console.log(array);
   for (i; i < array.length; i++) {
     const elcoso = array[i];
+
     const htmlagregar = `
-      <div class="kil">
-        <p>${elcoso}</p>
+      <div class="todolist">
+        <p>${elcoso.nombre}</p>
+        <p>${elcoso.date}</p>
         <button onclick="
-        array.splice(${i},1); 
-        addtarea();">cancelar</button>
+        array.splice(${i},1);
+        addtarea(); 
+        ">cancelar</button>
       </div>
-      
       `;
     todohtml += htmlagregar;
   }
-  document.querySelector(".log").innerHTML = todohtml;
+  document.querySelector(".ko").innerHTML = todohtml;
   console.log(array);
   inputElement.value = "";
 }
